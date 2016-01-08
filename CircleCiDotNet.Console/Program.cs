64f5efe0ac55d9c894d54356ef5fbe623fe721a7
Joe Rall <Joseph.Rall@Live.com>
2016-01-08 16:56:10 -0500

    Initial check-in of basic app.

---
+++ CircleCiDotNet.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CircleCiDotNet.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("I was just built with Circle Ci!");
+        }
+    }
+}
